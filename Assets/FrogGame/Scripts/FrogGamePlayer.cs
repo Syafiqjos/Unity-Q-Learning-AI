61ca91fe23a64af6d56f1f27fd3a3c383e65800e
Syafiqjos <syafiqjos@gmail.com>
2020-11-19 23:02:21 +0700

    Git Initial and Base 2 Input Frog ML Test

---
+++ Assets/FrogGame/Scripts/FrogGamePlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogGamePlayer : MonoBehaviour
+{
+    public FrogGameMaster gameMaster;
+
+    public float jumpForce;
+    public float raycastDownRange;
+
+    Rigidbody2D rigidbody2D;
+
+    public static  bool isGround = false;
+
+    public static bool CheckGround()
+    {
+        return isGround;
+    }
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        isGround = false;
+    }
+
+    public void Jump()
+    {
+        if (!FrogGameMaster.isGameOver) {
+            if (isGround)
+            {
+                rigidbody2D.velocity = new Vector2(0, jumpForce);
+                transform.position += new Vector3(0,2,0);
+                isGround = false;
+            }
+        }
+    }
+
+    bool firstOnce = false;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (firstOnce)
+        {
+            if (!isGround)
+            {
+                if (other.gameObject.tag == "Terrain")
+                {
+                    if (!other.gameObject.GetComponent<FrogGameTerrain>().isSafeTerrain)
+                    {
+                        FrogGameMaster.isGameOver = true;
+                        FrogGameMemory.AddMemory(FrogGameMaster.latestIndex0, FrogGameMaster.latestIndex1, FrogGameMaster.latestJumpType, false);
+                    }
+                    else
+                    {
+                        FrogGameMemory.AddMemory(FrogGameMaster.latestIndex0, FrogGameMaster.latestIndex1, FrogGameMaster.latestJumpType, true);
+                    }
+
+                    gameMaster.GetLatestIndex();
+                }
+                isGround = true;
+            }
+        }
+        else
+        {
+            firstOnce = true;
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit2D()
+    {
+        //isGround = false;
+    }
+}
---
+++ Assets/FrogGame/Scripts/FrogGameTerrain.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogGameTerrain : MonoBehaviour
+{
+    public int index = 0;
+
+    public bool isInitialized = false;
+    public Vector2 targetPos;
+
+    public bool isSafeTerrain = true;
+    public GameObject minion;
+
+    public void NextStep()
+    {
+        targetPos += new Vector2(FrogGameMaster.nextPositionDelta,0);
+    }
+
+    void Start()
+    {
+        if (!isInitialized)
+        {
+            targetPos = transform.position;
+            isInitialized = true;
+        }
+    }
+
+    public void Initialize(bool safe, int index)
+    {
+        this.index = index;
+
+        targetPos = transform.position;
+        isInitialized = true;
+        isSafeTerrain = safe;
+
+        if (isSafeTerrain)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        } else
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+        CheckDeath();
+
+        CheckCursor();
+    }
+
+    void CheckCursor()
+    {
+        minion.SetActive(FrogGameMaster.latestIndex0Terrain == index || FrogGameMaster.latestIndex1Terrain == index);
+    }
+
+    void Move()
+    {
+        if (targetPos != null)
+        {
+            transform.position = Vector2.Lerp(transform.position, targetPos, Time.deltaTime * 8);
+        }
+    }
+
+    void CheckDeath()
+    {
+        if (transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
